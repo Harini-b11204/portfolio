204680b0abc6d3e40ad91ff5004da0c926bffa4b
Harini Baskaran <harini.b11204@gmail.com>
2025-08-06 14:04:09 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,7 @@ export default function Projects() {
             <div className="p-10 bg-blue-200 flex flex-col justify-center items-center
                             gap-5 lg:flex-row max-w-5xl mx-auto shadow transition 
                             hover:shadow-xl hover:scale-[102%] w-full">
-                
+                <a href="http://github.com/Harini-b11204/ml-prediction-projects/tree/main/sentiment-analysis">
                 <div className="flex flex-col align-center mx-auto gap-4 justify-center flex-1">
                     <h2 className="font-bold text-3xl text-center text-blue-500">
                         Sentimental Analysis
@@ -22,13 +22,26 @@ export default function Projects() {
             <div className="p-10 bg-blue-200 flex flex-col justify-center items-center gap-5 
                             lg:flex-row-reverse max-w-5xl mx-auto shadow transition hover:shadow-xl 
                             hover:scale-[102%] w-full">
-                
+                <a href="http://github.com/Harini-b11204/ml-prediction-projects/tree/main/stock-market-prediction">
                 <div className="flex flex-col align-center mx-auto gap-4 justify-center flex-1">
-                    <h2 className="font-bold text-3xl text-center text-blue-500">
+                    <h2 className="font-bold text-3xl text-center text-blue-500" onclick=()>
                         Stock Market Analysis
                     </h2>
                     <p className="text-gray-700 text-lg" style={{fontFamily:'cursive'}}>
                         It uses the pandas library to analyze the stock market data and the matplotlib library to visualize the data.It helps in prediction of stock data
+                    </p>
+                </div>
+            </div>
+            <div className="p-10 bg-blue-200 flex flex-col justify-center items-center gap-5 
+                            lg:flex-row-reverse max-w-5xl mx-auto shadow transition hover:shadow-xl 
+                            hover:scale-[102%] w-full">
+                <a href="http://github.com/Harini-b11204/ml-prediction-projects/tree/main/house-price-prediction">
+                <div className="flex flex-col align-center mx-auto gap-4 justify-center flex-1">
+                    <h2 className="font-bold text-3xl text-center text-blue-500">
+                        House price prediction
+                    </h2>
+                    <p className="text-gray-700 text-lg" style={{fontFamily:'cursive'}}>
+                        This project uses historical stock data to predict future stock prices using LSTM and regression models.
                     </p>
                 </div>
             </div>
