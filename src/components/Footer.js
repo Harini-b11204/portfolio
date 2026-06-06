9c8525d3cb3f318dbda4ae24ea4286ec076f9f33
Harini Baskaran <harini.b11204@gmail.com>
2026-06-06 19:35:12 +0530

    Update copyright year in Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
     return (
         <section className="p-10 bg-blue-200 flex justify-center">
             <p className="font-bold text-gray-800" style={{fontFamily:'cursive'}}>
-                ©Copyright 2025. All rights are reserved by Harini
+                ©Copyright 2026. All rights are reserved by Harini
             </p>
         </section>
     );
