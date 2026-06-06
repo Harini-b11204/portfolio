012eb149bea388f1ecc7b8aaf096b0d3aac07512
Harini Baskaran <harini.b11204@gmail.com>
2026-06-06 19:23:50 +0530

    Remove professional skills section from Skills.js
    Removed the professional skills section from the Skills component.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,6 @@
 // Skills.js
 export default function Skills() {
     const skills = ["Python","Java","PostgreSQL", "HTML", "CSS", "Javascript", "Reactjs", "Nodejs","SQL","MongoDB","EDA","Data Visualization","Data Warehousing","Git","GitHub"];
-    const professionalSkills = ["Problem Solving", "Data Analysis", "Full-Stack Development", "API Development", "Database Design", "Project Coordination"];
     
     return (
         <section id="skills"
@@ -28,23 +27,7 @@ export default function Skills() {
                 </div>
             </div>
 
-            {/* Professional Skills */}
-            <div className="mt-10">
-                <h3 className="text-center text-2xl text-blue-500 font-semibold mb-5">Professional Skills</h3>
-                <div className="flex gap-4 justify-center 
-                                flex-wrap mx-auto max-w-xxl">
-                    {professionalSkills.map((skill, index) => {
-                        return (
-                            <div key={index}
-                                 className="cursor-pointer px-12 py-10 
-                                            rounded bg-blue-300 text-gray-700 text-lg 
-                                            flex items-center justify-center 
-                                            font-bold hover:shadow-xl hover:scale-105 transition" style={{fontFamily:'cursive'}}>
-                                {skill}
-                            </div>
-                        )})}
-                </div>
-            </div>
+           
         </section>
     )
 }
